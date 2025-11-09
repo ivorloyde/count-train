f38c8b8ccc279824be41f1a2c9739aab1224f863
ivorloyde <2560519711@qq.com>
2025-11-09 10:51:09 +0800

    减少worker

--- mytrain.py
+++ mytrain.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
         data=r"coco8.yaml",
         epochs=10,
         batch=-1,
-        imgsz=1280,
+        imgsz=640,
         cache="ram",
-        wokers=1,
     )

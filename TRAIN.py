296ce1facb62f04c2396cb96dae177ffb27a73da
UltralyticsAssistant <web@ultralytics.com>
2025-12-15 04:29:16 +0000

    Auto-format by https://ultralytics.com/actions

--- TRAIN.py
+++ TRAIN.py
@@ -1,14 +1,7 @@
-#这是一个用于训练YOLO模型的脚本，使用了自定义的数据集配置文件pollen.yaml。  
+# 这是一个用于训练YOLO模型的脚本，使用了自定义的数据集配置文件pollen.yaml。
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    model = YOLO(r'yolo11n.pt')
+    model = YOLO(r"yolo11n.pt")
 
-    model.train(
-        data=r"pollen.yaml",
-        epochs=3,
-        batch=-1,
-        imgsz=640,
-        cache="ram",
-        workers=1
-    )
+    model.train(data=r"pollen.yaml", epochs=3, batch=-1, imgsz=640, cache="ram", workers=1)
